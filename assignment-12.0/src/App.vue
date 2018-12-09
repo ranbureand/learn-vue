125b5b43cf77cb1e5e5b73d487473400a66e15de
Install Jekyll <iam-mac2@IAM-MAC2s-MacBook-Pro.local>
2018-12-09 19:34:53 +0300

    Update exercises and assignments.

--- assignment-12.0/src/App.vue
+++ assignment-12.0/src/App.vue
@@ -1,18 +1,49 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
-                <!-- Exercise -->
-                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <button
+          class="btn btn-primary"
+          v-custom-on:click="clicked"
+          >Click Me</button>
+        <hr>
+        <div
+          style="width: 100px; height: 100px; background-color: SpringGreen;"
+          v-custom-on:mouseenter="mouseEntered"
+          v-custom-on:mouseleave="mouseLeft"
+          ></div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {}
+  export default {
+    directives: {
+      'custom-on': {
+        bind(el, binding) {
+          /*el.onclick = function() {
+            binding.value;
+          }*/
+          const type = binding.arg;
+          const myFunction = binding.value;
+          el.addEventListener(type, myFunction);
+          // create a generic event listener
+        }
+      }
+    },
+    methods: {
+      clicked() {
+        console.log('Clicked.');
+      },
+      mouseEntered() {
+        console.log('Mouse entered.');
+      },
+      mouseLeft() {
+        console.log('Mouse left.');
+      }
+    }
+  }
 </script>
 
 <style>
--- exercise-12.0/src/App.vue
+++ exercise-12.0/src/App.vue
@@ -11,11 +11,13 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Global Directives</h1>
         <p v-highlight="'orange'">This text is colored.</p>
+        <!-- pass a string as the value -->
         <p v-highlight:background="'orange'">The background of this text is colored.</p>
         <p v-highlight:background.delayed="'orange'">The background of this text is colored, but delayed.</p>
         <!-- "highlight" is the directive, "background" is the argument, "delayed" is the modifier -->
         <h1>Custom Local Directives</h1>
         <p v-local-highlight="{ firstColor : 'orange'}">This text is colored.</p>
+        <!-- pass an object as the value -->
         <p v-local-highlight:background="{ firstColor : 'orange'}">The background of this text is colored.</p>
         <p v-local-highlight:background.blink="{ firstColor : 'orange', secondColor : 'green', delay : 500 }">The background of this text is colored and blinks.</p>
         <p v-local-highlight:background.delayed="{ firstColor : 'orange'}">The background of this text is colored, but delayed.</p>
